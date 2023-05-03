69be171a96bcd8088e76c68d8bf5a61480255b1d
Eunhae Hwang <71581199+rememberalltheanimals@users.noreply.github.com>
2023-05-02 19:13:59 -0500

    New member page

--- website/src/pages/Members.tsx
+++ website/src/pages/Members.tsx
@@ -8,99 +8,71 @@ function members() {
   }, []);
 
   return (
-    <main id="Members">
-      <section>
-    <div className="row">
-      <h1>Our Team</h1>
-    </div>
-    <div className="row">
-      {/* Column 1 */}
-      <div className="column">
-        <div className="card">
-          <div className="img_container">
-            <img src="#" />
+   <section className="team">
+    <div className="container">
+      <h1>Junior Programmers, JunCode</h1>
+      <div className="row">
+
+        <div className="col-md-3 profile text-center">
+          <div className="img-box">
+            <img src="../assets/jacoby.jpeg" className="img-responsive"/>
+            <ul>
+              <a href="https://www.linkedin.com/in/jacoby-yliniemi/" target="_blank"><li><i className="fa-brands fa-linkedin"></i></li></a>
+              <a href="https://github.com/Jacoby-Y" target="_blank"><li><i className="fa-brands fa-github"></i></li></a>
+              <a href="mailto:cobyyliniemi@gmail.com"><li><i className="fa-regular fa-envelope"></i></li></a>
+            </ul>
           </div>
-          <h3>Morgan Kruger</h3>
-          <p>Full stack dev</p>
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/morgan-kruger/" target="_blank">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/MorganKruger" target="_blank">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="mailto:nagromkruger@gmail.com">
-              <i className="fab fa-envelope"></i>
-            </a>
-          </div>
+          <h2>Jacoby Yliniemi</h2>
+          <h3>Full stack developer</h3>
+          <p>Node JS, Express, React, Mongo DB and more</p>
         </div>
-      </div>
-      {/* Column 2 */}
-      <div className="column">
-        <div className="card">
-          <div className="img_container">
-            <img src="../assets/jacoby.jpeg" />
+
+        <div className="col-md-3 profile text-center">
+          <div className="img-box">
+            <img src="../assets/Morgan.jpg" className="img-responsive"/>
+            <ul>
+              <a href="https://www.linkedin.com/in/morgan-kruger/" target="_blank"><li><i className="fa-brands fa-linkedin"></i></li></a>
+              <a href="https://github.com/MorganKruger" target="_blank"><li><i className="fa-brands fa-github"></i></li></a>
+              <a href="mailto:nagromkruger@gmail.com"><li><i className="fa-regular fa-envelope"></i></li></a>
+            </ul>
           </div>
-          <h3>Jacoby Yliniemi</h3>
-          <p>Full stack dev</p>
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/jacoby-yliniemi/" target="_blank">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/Jacoby-Y" target="_blank">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="mailto:cobyyliniemi@gmail.com">
-              <i className="fab fa-envelope"></i>
-            </a>
-          </div>
+          <h2>Morgan Kruger</h2>
+          <h3>Full stack developer</h3>
+          <p>Node JS, Express, React, Mongo DB and more</p>
         </div>
-      </div>
-      {/* Column 3 */}
-      <div className="column">
-        <div className="card">
-          <div className="img_container">
-            <img src="#" />
+
+        <div className="col-md-3 profile text-center">
+          <div className="img-box">
+            <img src="../assets/Davis.jpg" className="img-responsive"/>
+            <ul>
+              <a href="https://linkedin.com/in/djklevgard" target="_blank"><li><i className="fa-brands fa-linkedin"></i></li></a>
+              <a href="https://github.com/davisjklevgard"><li><i className="fa-brands fa-github"></i></li></a>
+              <a href="mailto:davisjklevgard@gmail.com"><li><i className="fa-regular fa-envelope"></i></li></a>
+            </ul>
           </div>
-          <h3>Davis Klevgard</h3>
-          <p>Full stack dev</p>
-          <div className="icons">
-            <a href="https://linkedin.com/in/djklevgard" target="_blank">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/davisjklevgard">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="mailto:davisjklevgard@gmail.com">
-              <i className="fab fa-envelope"></i>
-            </a>
-          </div>
+          <h2>Davis Klevgard</h2>
+          <h3>Full stack developer</h3>
+          <p>Node JS, Express, EJS, PostgreSQL and more</p>
         </div>
-      </div>
-      {/* Column 4 */}
-      <div className="column">
-        <div className="card">
-          <div className="img_container">
-            <img src="../assets/eunhae.png" />
+
+
+        <div className="col-md-3 profile text-center">
+          <div className="img-box">
+            <img src="../assets/eunhae.jpg" className="img-responsive"/>
+            <ul>
+              <a href="https://www.linkedin.com/in/eunhae-hwang-4a4282261/" target="_blank"><li><i className="fa-brands fa-linkedin"></i></li></a>
+              <a href="https://github.com/rememberalltheanimals" target="_blank"><li><i className="fa-brands fa-github"></i></li></a>
+              <a href="mailto:lavieeunhae@gmail.com"><li><i className="fa-regular fa-envelope"></i></li></a>
+            </ul>
           </div>
-          <h3>Eunhae Hwang</h3>
-          <p>Full stack dev</p>
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/eunhae-hwang-4a4282261/" target="_blank">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/rememberalltheanimals" target="_blank">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="mailto:lavieeunhae@gmail.com">
-              <i className="fab fa-envelope"></i>
-            </a>
-          </div>
+          <h2>Eunhae Hwang</h2>
+          <h3>Full stack developer</h3>
+          <p>ASP.NET Web Forms, MVC, Java, MySQL, PHP and more</p>
         </div>
+
       </div>
     </div>
-  </section>
-    </main>
+   </section>
   )
 }
 
